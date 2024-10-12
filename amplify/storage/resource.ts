622764b096588ecd07f7114c1415f2ec338d0244
William Ong <wlo12489@gmail.com>
2024-10-12 16:27:11 -0700

    Edit profile w/o user data

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,14 @@
+<div className="mx-auto h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
+<div className="lg:flex lg:justify-between lg:gap-4">
+
+  <div className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-5/12 lg:flex-col lg:justify-between lg:py-20">
+    <Profile />
+  </div>
+  
+  <div className="flex flex-col lg:w-7/12 lg:pb-6">
+    <div id="about"> <About /> </div>
+    <div id="experience"> <Experience /> </div>
+    <div id="projects"> <Projects /> </div>
+    <div id="education"> <Education /> </div>
+    <Footer />
+  </div>
--- app/components/profile.tsx
+++ app/components/profile.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import Image from "next/image";
-import React, { MouseEventHandler, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import placeholderImg from '@/app/public/placeholder-pfp.svg';
+import editImg from '@/app/public/edit-pfp.svg';
 import { Modal, ModalContent, ModalHeader, ModalBody, Button, ModalFooter, useDisclosure } from "@nextui-org/react";
 import { TextField, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Avatar} from "@mui/material";
 
@@ -50,6 +51,10 @@ export default function Profile() {
     setName(event.target.value);
   };
 
+  const pictureChange = () => {
+    // TODO: Picture change
+  };
+
   const bioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setBio(event.target.value);
   };
@@ -74,10 +79,9 @@ export default function Profile() {
     <div className="flex flex-row items-center gap-2">
       <div> {user.name} </div>
         <Avatar
-          className="w-8 h-8 text-lg hover:cursor-pointer"
+          className="w-8 h-8 text-lg hover:cursor-pointer hover:scale-110"
           onClick={onOpen}
           alt=''
-          src={placeholderImg}
         />
     </div>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
@@ -86,21 +90,29 @@ export default function Profile() {
             <>
               <ModalHeader className="flex flex-col gap-1 text-black text-lg font-bold"> Edit Profile </ModalHeader>
 
-              <ModalBody className="text-black flex relative">
-                <Avatar className="w-16 h-16" alt="" src={placeholderImg} />
-                <TextField label="Name" value={name} variant="outlined" onChange={nameChange}/>
-                <TextField label="Bio" value={bio} variant="outlined" onChange={bioChange}/>
-                <TextField label="LinkedIn" value={linkedin} variant="outlined" onChange={linkedinChange} />
-                <FormControl>
-                  <FormLabel> Privacy </FormLabel>
-                  <RadioGroup
-                    value={privacy}
-                    onChange={privacyChange}
-                  >
-                    <FormControlLabel value="Private" control={<Radio />} label="Private" />
-                    <FormControlLabel value="Public" control={<Radio />} label="Public" />
-                  </RadioGroup>
-                </FormControl>
+              <ModalBody className="text-black flex relative items-center">
+                <div className="flex-shrink-0 mb-4 relative">
+                  <Avatar
+                    className="w-32 h-32 mr-4 hover:cursor-pointer"
+                    alt=""
+                    onClick={pictureChange}
+                  />
+                </div>
+                <div className="flex flex-col gap-4 w-full">
+                  <TextField label="Name" value={name} variant="outlined" onChange={nameChange}/>
+                  <TextField label="Bio" value={bio} variant="outlined" onChange={bioChange}/>
+                  <TextField label="LinkedIn" value={linkedin} variant="outlined" onChange={linkedinChange} />
+                  <FormControl>
+                    <FormLabel> Privacy </FormLabel>
+                    <RadioGroup
+                      value={privacy}
+                      onChange={privacyChange}
+                    >
+                      <FormControlLabel value="Private" control={<Radio />} label="Private" />
+                      <FormControlLabel value="Public" control={<Radio />} label="Public" />
+                    </RadioGroup>
+                  </FormControl>
+                </div>
               </ModalBody>
 
               <ModalFooter>
