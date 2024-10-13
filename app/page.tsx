fd591917f9616f7f3d27aac585753956369c7366
anmolkb <anmolbhuller21@gmail.com>
2024-10-12 17:10:40 -0700

    added welcome message to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,30 @@
 export default function Home() {
   return (
     // TODO: Change colors if needed
-    <div className="bg-dark_purple flex h-12 w-full items-center">
+    <div>
+      <div className="bg-dark_purple flex h-12 w-full items-center">
 
-      <div className="mr-auto pl-8">
-        [logo] [app_name]
+        <div className="mr-auto pl-8">
+          [logo] [app_name]
+        </div>
+
+        <div className="mr-auto ml-auto px-4 flex flex-row gap-6">
+          <a href="home_page" className="hover:text-light_purple"> Home </a>
+
+          <a href="your_courses" className="hover:text-light_purple"> Your Courses </a>
+          <a href="peer_connect" className="hover:text-light_purple"> Peer Connect </a>
+        </div>
+        
+        <div className="ml-auto pr-8">
+          [name] [pfp]
+        </div>
       </div>
 
-      <div className="mr-auto ml-auto px-4 flex flex-row gap-6">
-        <a href="#" className="hover:text-light_purple"> Home </a>
-        <a href="your_courses" className="hover:text-light_purple"> Your Courses </a>
-        <a href="peer_connect" className="hover:text-light_purple"> Peer Connect </a>
+      <div className="text-left mt-8">
+        <h1 className="text-5xl font-bold mx-8">Welcome, [user]!</h1>
       </div>
 
-      <div className="ml-auto pr-8">
-        [name] [pfp]
-      </div>
     </div>
+    
   );
 }
